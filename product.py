54f50b623377a98b883770c58ada946945150b74
celm1990 <celm1990@hotmail.com>
2018-02-28 18:09:16 -0500

    FIX: al buscar categorias publicas, solo tomar 1 en caso de haber mas con el mismo nombre

--- product.py
+++ product.py
@@ -163,7 +163,7 @@ class product_product(models.Model):
                         fullname = fullname + "/" + path["name"]
                         www_cats = self.env['product.public.category']
                         if www_cats!=False:
-                            www_cat_id = www_cats.search([('name','=',path["name"])]).id
+                            www_cat_id = www_cats.search([('name','=',path["name"])], limit=1).id
                             if www_cat_id==False:
                                 www_cat_fields = {
                                   'name': path["name"],
