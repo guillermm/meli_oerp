08ccc07c1f60e543b8451c5a49f3423ba2a35bf9
celm1990 <celm1990@hotmail.com>
2018-03-09 15:10:09 -0500

    ADD: clase para guardar los atributos y valores que cada categoria soporta(Primer paso para poder subir atributos y variantes en las publicaciones)

--- models/category.py
+++ models/category.py
@@ -25,6 +25,67 @@ from odoo import fields, osv, models, api
 
 _logger = logging.getLogger(__name__)
 
+class MercadolibreCategoryAttributeValue(models.Model):
+
+    _name = 'mercadolibre.category.attribute.value'
+    _description = u'Opciones de atributos'
+    
+    name = fields.Char(string=u'Nombre')
+    code = fields.Char(string=u'ID MELI')
+    
+    @api.model
+    def find_or_create(self, name, code):
+        value_find = self.search([
+            ('name','=', name),
+            ('code','=', code),
+        ], limit=1)
+        if not value_find:
+            value_find = self.create({
+                'name': name,
+                'code': code,
+            })
+        return value_find
+    
+class MercadolibreCategoryAttribute(models.Model):
+
+    _name = 'mercadolibre.category.attribute'
+    _description = u'mercadolibre.category.attribute'
+    
+    name = fields.Char(string=u'Nombre')
+    code = fields.Char(string=u'ID MELI')
+    attribute_type = fields.Selection([
+        ('string','Texto Libre'),
+        ('number','Numeros'),
+        ('number_unit','Unidad de Medida'),
+        ('list','Lista de Opciones'),
+        ('boolean','Si/No'),
+        ], string=u'Tipo de Atributo')
+    value_ids = fields.Many2many('mercadolibre.category.attribute.value', 'mercadolibre_category_attribute_value_rel', 
+        'attribute_id', 'value_id', u'Valores de Atributo')
+    
+    @api.model
+    def find_or_create(self, name, code, attribute_type, value_list=None):
+        if not value_list:
+            value_list = []
+        value_model = self.env['mercadolibre.category.attribute.value']
+        attribute_find = self.search([
+            ('name','=', name),
+            ('code','=', code),
+            ('attribute_type','=', attribute_type),
+        ], limit=1)
+        if not attribute_find:
+            value_ids = []
+            for value in value_list:
+                value_find = value_model.find_or_create(value.get('name') or 'SN', value.get('id') or 'SC',)
+                value_ids.append(value_find.id)
+            attribute_find = self.create({
+                'name': name,
+                'code': code,
+                'attribute_type': attribute_type,
+                'value_ids': [(6, 0, value_ids)],
+            })
+        return attribute_find
+    
 class MercadolibreCategory(models.Model):
     _name = "mercadolibre.category"
     _description = "Categories of MercadoLibre"
@@ -33,36 +94,52 @@ class MercadolibreCategory(models.Model):
     meli_category_id = fields.Char('Category Id')
     public_category_id = fields.Integer('Public Category Id')
     listing_allowed = fields.Boolean(u'Permitido Postear productos?')
+    attribute_ids = fields.Many2many('mercadolibre.category.attribute', 'mercadolibre_category_attribute_rel', 
+        'category_id', 'attribute_id', u'Atributos')
 
-    def import_category(self, category_id ):
-        category_model = self.env['mercadolibre.category']
+    def import_category(self, category_id):
+        attribute_model = self.env['mercadolibre.category.attribute']
         meli_util_model = self.env['meli.util']
         meli = meli_util_model.get_new_instance()
-        if (category_id):
-            ml_cat_id = category_model.search([('meli_category_id','=',category_id)])
-            if (ml_cat_id):
-                print "category exists!" + str(ml_cat_id)
-            else:
-                print "Creating category: " + str(category_id)
-                #https://api.mercadolibre.com/categories/MLA1743
-                response_cat = meli.get("/categories/"+str(category_id), {'access_token':meli.access_token})
-                rjson_cat = response_cat.json()
-                print "category:" + str(rjson_cat)
-                fullname = ""
-                if ("path_from_root" in rjson_cat):
-                    path_from_root = rjson_cat["path_from_root"]
-                    for path in path_from_root:
-                        fullname = fullname + "/" + path["name"]
+        meli_categ = self.browse()
+        if not category_id:
+            return meli_categ
+        meli_categ = self.search([('meli_category_id','=',category_id)], limit=1)
+        if (meli_categ):
+            _logger.info("category exists! %s ID: %s" % (category_id, meli_categ.id))
+            return meli_categ
+        else:
+            _logger.info("Creating category: %s" % (category_id))
+            #https://api.mercadolibre.com/categories/MLA1743
+            response_cat = meli.get("/categories/%s" % (category_id), {'access_token':meli.access_token})
+            rjson_cat = response_cat.json()
+            _logger.info("category:" + str(rjson_cat))
+            fullname = ""
+            if ("path_from_root" in rjson_cat):
+                path_from_root = rjson_cat["path_from_root"]
+                for path in path_from_root:
+                    fullname = fullname + "/" + path["name"]
 
-                #fullname = fullname + "/" + rjson_cat['name']
-                #print "category fullname:" + str(fullname)
-                _logger.info(fullname)
-                cat_fields = {
-                    'name': fullname,
-                    'meli_category_id': ''+str(category_id),
-                    'listing_allowed': rjson_cat.get('settings', {}).get('listing_allowed') or False,
-                    }
-                ml_cat_id = category_model.create((cat_fields))
+            #fullname = fullname + "/" + rjson_cat['name']
+            #print "category fullname:" + str(fullname)
+            _logger.info(fullname)
+            response_attributes = meli.get("/categories/%s/attributes" % (category_id), {'access_token':meli.access_token})
+            attributes_json = response_attributes.json()
+            attribute_ids = []
+            for attribute in attributes_json:
+                attribute = attribute_model.find_or_create(attribute.get('name') or 'SN',
+                                                           attribute.get('id') or 'SC', 
+                                                           attribute.get('value_type') or 'string', 
+                                                           attribute.get('values') or attribute.get('allowed_units') or [])
+                attribute_ids.append(attribute.id)
+            cat_fields = {
+                'name': fullname,
+                'meli_category_id': ''+str(category_id),
+                'listing_allowed': rjson_cat.get('settings', {}).get('listing_allowed') or False,
+                'attribute_ids': [(6, 0, attribute_ids)],
+                }
+            meli_categ = self.create((cat_fields))
+            return meli_categ
 
 
     def import_all_categories(self, category_root ):
